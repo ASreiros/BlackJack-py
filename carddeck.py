9c2aa483d7d6dfbec403def53351cf2d5c17dfd9
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2023-02-24 18:51:06 +0200

    21 blackjack is done

---
+++ carddeck.py
@@ -0,0 +1,217 @@
+deck = [
+    {
+        "name": "ACE(+)",
+        "value": 11,
+    },
+    {
+        "name": "KING(+)",
+        "value": 10,
+    },
+    {
+        "name": "QUEEN(+)",
+        "value": 10,
+    },
+    {
+        "name": "JACK(+)",
+        "value": 10,
+    },
+    {
+        "name": "10(+)",
+        "value": 10,
+    },
+    {
+        "name": "9(+)",
+        "value": 9,
+    },
+    {
+        "name": "8(+)",
+        "value": 8,
+    },
+    {
+        "name": "7(+)",
+        "value": 7,
+    },
+    {
+        "name": "6(+)",
+        "value": 6,
+    },
+    {
+        "name": "5(+)",
+        "value": 5,
+    },
+    {
+        "name": "4(+)",
+        "value": 4,
+    },
+    {
+        "name": "3(+)",
+        "value": 3,
+    },
+
+    {
+        "name": "2(+)",
+        "value": 2,
+    },
+    {
+        "name": "ACE(H)",
+        "value": 11,
+    },
+    {
+        "name": "KING(H)",
+        "value": 10,
+    },
+    {
+        "name": "QUEEN(H)",
+        "value": 10,
+    },
+    {
+        "name": "JACK(H)",
+        "value": 10,
+    },
+    {
+        "name": "10(H)",
+        "value": 10,
+    },
+    {
+        "name": "9(H)",
+        "value": 9,
+    },
+    {
+        "name": "8(H)",
+        "value": 8,
+    },
+    {
+        "name": "7(H)",
+        "value": 7,
+    },
+    {
+        "name": "6(H)",
+        "value": 6,
+    },
+    {
+        "name": "5(H)",
+        "value": 5,
+    },
+    {
+        "name": "4(H)",
+        "value": 4,
+    },
+    {
+        "name": "3(H)",
+        "value": 3,
+    },
+
+    {
+        "name": "2(H)",
+        "value": 2,
+    },
+    
+    {
+        "name": "ACE(S)",
+        "value": 11,
+    },
+    {
+        "name": "KING(S)",
+        "value": 10,
+    },
+    {
+        "name": "QUEEN(S)",
+        "value": 10,
+    },
+    {
+        "name": "JACK(S)",
+        "value": 10,
+    },
+    {
+        "name": "10(S)",
+        "value": 10,
+    },
+    {
+        "name": "9(S)",
+        "value": 9,
+    },
+    {
+        "name": "8(S)",
+        "value": 8,
+    },
+    {
+        "name": "7(S)",
+        "value": 7,
+    },
+    {
+        "name": "6(S)",
+        "value": 6,
+    },
+    {
+        "name": "5(S)",
+        "value": 5,
+    },
+    {
+        "name": "4(S)",
+        "value": 4,
+    },
+    {
+        "name": "3(S)",
+        "value": 3,
+    },
+
+    {
+        "name": "2(S)",
+        "value": 2,
+    },
+
+    {
+        "name": "ACE(D)",
+        "value": 11,
+    },
+    {
+        "name": "KING(D)",
+        "value": 10,
+    },
+    {
+        "name": "QUEEN(D)",
+        "value": 10,
+    },
+    {
+        "name": "JACK(D)",
+        "value": 10,
+    },
+    {
+        "name": "10(D)",
+        "value": 10,
+    },
+    {
+        "name": "9(D)",
+        "value": 9,
+    },
+    {
+        "name": "8(D)",
+        "value": 8,
+    },
+    {
+        "name": "7(D)",
+        "value": 7,
+    },
+    {
+        "name": "6(D)",
+        "value": 6,
+    },
+    {
+        "name": "5(D)",
+        "value": 5,
+    },
+    {
+        "name": "4(D)",
+        "value": 4,
+    },
+    {
+        "name": "3(D)",
+        "value": 3,
+    },
+
+    {
+        "name": "2(D)",
+        "value": 2,
+    },
+    
+]
---
+++ main.py
@@ -0,0 +1,116 @@
+import copy
+import random
+
+import logo
+import  carddeck
+from copy import deepcopy
+
+my_deck = []
+
+
+def deal_card():
+    card = random.choice(my_deck)
+    my_deck.remove(card)
+    return card
+
+def blackjack():
+    print(logo.logo)
+    print("     Welcome to BLACKJACK        ")
+    player_card_names = []
+    player_card_values = []
+    dealer_card_names = []
+    dealer_card_values = []
+    for n in range(2):
+        player_card = deal_card()
+        dealer_card = deal_card()
+        player_card_names.append(player_card['name'])
+        player_card_values.append(player_card['value'])
+        dealer_card_names.append(dealer_card['name'])
+        dealer_card_values.append(dealer_card['value'])
+        dealer_card_names_hidden = copy.deepcopy(dealer_card_names)
+        dealer_card_values_hidden = copy.deepcopy(dealer_card_values)
+        dealer_card_names_hidden[0] = "*"
+        dealer_card_values_hidden[0] = "*"
+
+    player_stop = False
+    dealer_stop = False
+
+    while player_stop is False:
+        aces = True
+        while sum(player_card_values) > 21 and aces is True:
+            if 11 in player_card_values:
+                player_card_values[player_card_values.index(11)] = 1
+            else:
+                aces = False
+                player_stop = True
+
+        print(f"Your cards:        {player_card_names}")
+        print(f"Your cards values: {player_card_values}, your total :{sum(player_card_values)}")
+        print("         ")
+        print(f"Dealer cards:        {dealer_card_names_hidden}")
+        print(f"Dealer cards values: {dealer_card_values_hidden}, dealer total :{dealer_card_values_hidden[1]}")
+
+        if sum(player_card_values) <= 21:
+            if input("Type 'y' to get another card, type 'n' to pass: ") == "y":
+                player_card = deal_card()
+                player_card_names.append(player_card['name'])
+                player_card_values.append(player_card['value'])
+                print("-------------------------------------")
+            else:
+                player_stop = True
+        else:
+            print("You went over the top")
+
+    while dealer_stop is False:
+        aces = True
+        while sum(dealer_card_values) > 21 and aces is True:
+            if 11 in dealer_card_values:
+                dealer_card_values[dealer_card_values.index(11)] = 1
+            else:
+                aces = False
+                dealer_stop = True
+
+        print(f"Your cards:        {player_card_names}")
+        print(f"Your cards values: {player_card_values}, your total :{sum(player_card_values)}")
+        print("         ")
+        print(f"Dealer cards:        {dealer_card_names}")
+        print(f"Dealer cards values: {dealer_card_values}, dealer total :{sum(dealer_card_values)}")
+
+        if sum(player_card_values) > 21:
+            dealer_stop = True
+        elif sum(dealer_card_values) <= 21:
+            if sum(dealer_card_values) < 17:
+                dealer_card = deal_card()
+                dealer_card_names.append(dealer_card['name'])
+                dealer_card_values.append(dealer_card['value'])
+                print("-------------------------------------")
+            else:
+                dealer_stop = True
+        else:
+            print("Dealer went over the top")
+
+    result_text = ""
+    if sum(player_card_values) > 21:
+        result_text = "You lose. You went over 21."
+    elif sum(dealer_card_values) > 21:
+        result_text = "You win. Dealer went over 21."
+    elif sum(dealer_card_values) > sum(player_card_values):
+        result_text = "You lost. Dealer has more points."
+    elif sum(dealer_card_values) < sum(player_card_values):
+        result_text = "You Win. You have more points."
+    elif sum(dealer_card_values) == sum(player_card_values):
+        result_text = "It is a DRAW. You have same amount of points."
+    else:
+        print("Something was not planned. Else clause")
+
+    print(f"Your total score is {sum(player_card_values)}. Dealer total score is {sum(dealer_card_values)}. {result_text}")
+
+
+play = True
+while play:
+    if input("Do you want to play the BLACKJACK game? Please type 'y' for yes or 'n' for no: ") == 'y':
+        my_deck = copy.deepcopy(carddeck.deck)
+        blackjack()
+    else:
+        print("Have a good day. Program will exit now")
+        play = False
